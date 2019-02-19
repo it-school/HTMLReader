5778ad1e3d769af21b43d7abb9dbd9e82af759f3
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2019-02-20 01:33:26 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,10 @@ import org.jsoup.select.Elements;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         String url = "https://www.bbc.com/news";
-        System.out.println("Fetching %s..."  + url);
+        System.out.println("Fetching data from URI: "  + url);
 
         References hlinks = new References();
-
 
         int n = 0;
         try
@@ -32,7 +30,6 @@ public class Main {
                         Element a = ul.child(0);
                         hlinks.list.add(new Reference(a.text(), a.attr("href")));
                         //System.out.println(a.text() + '\t' + a.attr("href"));
-
                     }
                 }
             }
@@ -42,38 +39,10 @@ public class Main {
                 if (r.text.contains("r"))
                 System.out.println(r);
             }
-        /*
-        Elements links = doc.select("a[href]");
-        Elements media = doc.select("[src]");
-        Elements imports = doc.select("link[href]");
-
-        print("\nMedia: (%d)", media.size());
-        for (Element src : media) {
-            if (src.tagName().equals("img"))
-                print(" * %s: <%s> %sx%s (%s)",
-                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
-                        trim(src.attr("alt"), 20));
-            else
-                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
-        }
-
-        print("\nImports: (%d)", imports.size());
-        for (Element link : imports) {
-            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
-        }
-
-        print("\nLinks: (%d)", links.size());
-        for (Element link : links) {
-            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
-        }
-*/
-
         }
         catch(Exception exc)
         {
-            System.out.println("Error loading page");
+            System.out.println("Page loading error!!!");
         }
     }
-
 }
-
